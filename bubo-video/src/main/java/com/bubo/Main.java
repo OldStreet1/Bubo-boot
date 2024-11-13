d65291f868c81f181b49ca875ab588c0d173ab42
oldstreet1 <hjj1010>
2024-11-13 09:37:11 +0800

    视频监控模块

---
+++ bubo-video/src/main/java/com/bubo/Main.java
@@ -0,0 +1,12 @@
+package com.bubo;
+
+/**
+ * @author: Hongjj
+ * @date: 2024/11/13 09:33
+ * @desc:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
